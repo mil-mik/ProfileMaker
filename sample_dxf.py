222e71a15d6ac98ffc5c62b7ba5dda7df2c40b07
Milosz Mika <milosz.mika93@gmail.com>
2024-04-23 19:46:42 +0200

    draw simple .dxf file

---
+++ sample_dxf.py
@@ -0,0 +1,26 @@
+import ezdxf
+from ezdxf import colors
+from ezdxf.enums import TextEntityAlignment
+
+# Create a new DXF document.
+doc = ezdxf.new(dxfversion="R2010")
+
+# Create new table entries (layers, linetypes, text styles, ...).
+doc.layers.add("TEXTLAYER", color=colors.RED)
+
+# DXF entities (LINE, TEXT, ...) reside in a layout (modelspace,
+# paperspace layout or block definition).
+msp = doc.modelspace()
+
+# Add entities to a layout by factory methods: layout.add_...()
+msp.add_line((0, 0), (10, 0), dxfattribs={"color": colors.YELLOW})
+msp.add_line((10, 0), (15, 0), dxfattribs={"color": colors.BLUE})
+msp.add_line((12, 0), (20, 0), dxfattribs={"color": colors.YELLOW})
+msp.add_text(
+    "Test",
+    dxfattribs={
+        "layer": "TEXTLAYER"
+    }).set_placement((0, 0.2), align=TextEntityAlignment.CENTER)
+
+# Save the DXF document.
+doc.saveas("test.dxf")
